1129cffe4bb96eb4ca66d3b4525244c870190f2c
hyung-2 <nh_0221@naver.com>
2023-11-28 18:04:51 +0900

    추가: 깃허브 링크 추가

--- src/app/read/contact/page.js
+++ src/app/read/contact/page.js
@@ -6,7 +6,7 @@ function contact(props){
       <h2>contact page</h2>
       <h4>연락처:</h4>
       <h4>이메일:</h4>
-      <h4>깃허브:</h4>
+      <h4>깃허브: <a href="https://github.com/hyung-2" target='_blank'>https://github.com/hyung-2</a></h4>
     </>
   )
 }
