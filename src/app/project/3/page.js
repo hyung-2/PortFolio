7dd7902e3f2038603561519af6079474817acad2
hyung-2 <nh_0221@naver.com>
2023-12-01 13:39:13 +0900

    수정:프로젝트3 상세페이지 내용수정

--- src/app/project/3/page.js
+++ src/app/project/3/page.js
@@ -29,11 +29,21 @@ export default function page(){
         </div>
         <div className='descriptionBox'>
           <h4 className='project-subtitle font'>23.11.01 ~ 23.11.20 (4인) </h4>
-          {/* <p>HTML, CSS, JAVAscript를 기반으로 만든 웹사이트입니다.</p> */}
-          <p className='font'>리액트 네이티브를 기반으로 만든 안드로이드 앱입니다.</p>
-          <p className='font'>사용자와 친구추가 후에 캘린더를 통해 일정을 공유하고 채팅을 하고, 알람을 설정할수 있습니다.<br/>
-          저는 캘린더 부분을 담당했습니다.
-          </p>
+          <div className='description'>
+            <div className='aboutProject'>
+              <h3 className='des-title font'>프로젝트소개</h3>
+              <div className='font'>- 리액트 네이티브를 활용한 세번째 프로젝트</div>
+              <div className='font'>- 친구와 일정 공유, 채팅, 알람설정</div>
+              <div className='font'>- 캘린더 부분 담당</div>
+            </div>
+            <div>
+              <h3 className='font'>주요 기능</h3>
+              <div className='font'>- 회원가입 & 로그인</div>
+              <div className='font'>- 일정 추가, 수정, 삭제</div>
+              <div className='font'>- 해당 일정 멤버들과 채팅</div>
+              <div className='font'>- 알람</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
