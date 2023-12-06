3c5394124932a3e52835fb92f66b830a7993abfa
hyung-2 <nh_0221@naver.com>
2023-12-06 17:47:18 +0900

    수정:레이아웃 수정중

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,23 +1,36 @@
+'use client'
+
+import { useState } from "react";
+
 import { AiFillTag } from "react-icons/ai";
 import { AiFillMail } from "react-icons/ai";
 import { AiFillGithub } from "react-icons/ai";
+import { FaBirthdayCake } from "react-icons/fa";
 
 
 function about(props){
   // console.log(props)
+  const [typing, setTyping] = useState(false)
+
   return(
     <>
       <h2 className="pageTitle font">About me</h2>
       <div className="about">
-        <div className="photobox">
-          <img src="/profile.png"/>
-        </div>
-        <div className="contentBox">
-          <div className="introBox">
-            <p className="intro font">안녕하세요. <br/>
-            새로 배우는 것이 재미있고 그 배움을 토대로 도전하는 것이 즐거운 <span className="name">안남형</span> 입니다. <br/>
-            현재 프론트엔드 개발자를 희망하고 있습니다. </p>
+        <div className="contentBox aniShowDown">
+          <div className="flex">
+            <AiFillTag/>
+            <p className="contact-item font name">안남형</p>
           </div>
+          <div className="flex">
+            <FaBirthdayCake/>
+            <p className="contact-item font name">1994.02.21</p>
+          </div>
+        </div>
+        <div className="photobox">
+          {/* <img src="/profile.png"/> */}
+        </div>
+        <div className="contentBox aniShowDown">
+          <div className="font name">Contact!</div>
           <div className="contact">
             <div className="flex">
               <AiFillMail/>
@@ -25,10 +38,15 @@ function about(props){
             </div>
             <div className="flex">
               <AiFillGithub/>
-              <p className="contact-item font">깃허브: <a href="https://github.com/hyung-2" target='_blank'>https://github.com/hyung-2</a></p>
+              <p className="contact-item font">깃허브: <a href="https://github.com/hyung-2" target='_blank' title="깃허브로 이동">hyung-2</a></p>
             </div>
           </div>
         </div>
+      </div>
+      <div className="introBox">
+        <p className="intro font">안녕하세요. <br/>
+        새로 배우는 것이 재미있고 그 배움을 토대로 도전하는 것이 즐거운 <span className="">안남형</span> 입니다. <br/>
+        현재 프론트엔드 개발자를 희망하고 있습니다. </p>
       </div>
     </>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,15 @@
-'use client'
 
-import { useState } from "react";
 import Link from "next/link";
 
 export default function Home() {
 
-  const click = () => {
-    console.log('클릭')
-  }
   return (
-    <div className="home" onClick={click}>
+    <div className="home">
       <Link className="link" href={`/about`}>
-        <div className="imgBox">
+        {/* <div className="imgBox"> */}
           {/* <img src='/text.png' alt="main"></img> */}
           <h2 className="homeTitle">Namhyung's Portfolio</h2>
-        </div>
+        {/* </div> */}
       </Link>
     </div>
   )
